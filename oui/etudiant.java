fa29c72f8408d0a8bafa9cf6393807bc2b71e5e7
Adnan EL KHARRAZ <2412adelkharraz@student.epfc.eu>
2022-12-05 18:51:48 +0100

    first commit

---
+++ oui/etudiant.java
@@ -0,0 +1,17 @@
+package oui;
+
+public class etudiant {
+    String nom ;
+    String prénom;
+    int identifiant;
+    String cours;
+    int année;
+    String groupe;
+    String titulaire;
+    public  String ewi(){
+        
+        return cours;   
+    }
+
+}
+
